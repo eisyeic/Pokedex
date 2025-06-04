4d37e2b3b271aada7ab6ecdb78773836b0f5502f
David Eisenbarth <davideisenbarth@gmail.com>
2025-06-04 14:19:13 +0200

    pokemon filter is complet and loading icon isavailable

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let pokemon = [];
 
 function init() {
     loadAPI();
+
 }
 
 function loadMorePokemon() {
@@ -16,15 +17,16 @@ function loadMorePokemon() {
 
 async function loadAPI() {
     try {
+        showLoadingScreen();
         BASE_URL = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
         const response = await fetch(BASE_URL);
         const data = await response.json();
         const results = data.results;
-        
+
         for (let index = 0; index < results.length; index++) {
             const pokemonResponse = await fetch(results[index].url);
             const pokemonData = await pokemonResponse.json();
-            
+
             pokemon.push({
                 name: results[index].name,
                 img: pokemonData.sprites.other.home.front_default,
@@ -34,15 +36,73 @@ async function loadAPI() {
                 weight: pokemonData.weight
             });
         }
-        
+
         render();
+        hideLoadingScreen();
     } catch (error) {
         console.error('Fehler beim Abrufen der Daten:', error);
+        hideLoadingScreen();
     }
 }
 
 function render() {
-    getRenderPokemon();
+    const mainElement = document.getElementById('main-elements');
+    mainElement.innerHTML = '';
+
+    for (let index = 0; index < pokemon.length; index++) {
+        renderPokemonCard(index);
+    }
 }
 
+
+function showLoadingScreen() {
+    document.getElementById('load-window-container').classList.remove('d-none');
+}
+
+function hideLoadingScreen() {
+    document.getElementById('load-window-container').classList.add('d-none');
+}
+
+async function filterPokemon() {
+    const searchInput = document.getElementById('search-input');
+    const searchTerm = searchInput.value.toLowerCase();
+
+    if (searchTerm === '') {
+        offset = 0;
+        pokemon = [];
+        loadAPI();
+    } else {
+        showLoadingScreen();
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1000`);
+            const data = await response.json();
+            const allResults = data.results;
+
+            const filteredResults = allResults.filter(p =>
+                p.name.toLowerCase().startsWith(searchTerm)
+            );
+
+            pokemon = [];
+
+            for (let i = 0; i < filteredResults.length; i++) {
+                const pokemonResponse = await fetch(filteredResults[i].url);
+                const pokemonData = await pokemonResponse.json();
+
+                pokemon.push({
+                    name: filteredResults[i].name,
+                    img: pokemonData.sprites.other.home.front_default,
+                    types: pokemonData.types,
+                    id: pokemonData.id,
+                    height: pokemonData.height,
+                    weight: pokemonData.weight
+                });
+            }
+
+            render();
+        } catch (error) {
+            console.error('Fehler bei der Suche:', error);
+        }
+        hideLoadingScreen();
+    }
+}
 
--- scripts/templates.js
+++ scripts/templates.js
@@ -22,9 +22,25 @@ function getRenderPokemon() {
 function getRenderTypesElements(index) {
     for (let i = 0; i < pokemon[index].types.length; i++) {
         document.getElementById(`types-elements-${index}`).innerHTML +=
-            `<div class="types-icon background-${pokemon[index].types[0].type.name}">
+            `<div class="types-icon background-${pokemon[index].types[i].type.name}">
             <img src="./assers/icons/${pokemon[index].types[i].type.name}.svg">
             </div>`
     }
 }
 
+function renderPokemonCard(index) {
+    const mainElement = document.getElementById('main-elements');
+    mainElement.innerHTML +=
+        `<div class="pokemon-container">
+            <div class="pokemon-header">
+                <h1>#${pokemon[index].id}</h1>
+                <h1>${pokemon[index].name.charAt(0).toUpperCase() + pokemon[index].name.slice(1)}</h1>
+            </div>
+            <a class="pokemon-img background-${pokemon[index].types[0].type.name}"><img src="${pokemon[index].img}"></a>
+            <div class="types-container-icons" id="types-elements-${index}">
+                
+            </div>
+        </div>`;
+
+    getRenderTypesElements(index);
+}
